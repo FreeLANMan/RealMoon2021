ab3e41d14ba18b5d846aa400c78494f750a0931b
FreeLANMan <43796655+FreeLANMan@users.noreply.github.com>
2021-12-06 17:40:09 -0300

    Add files via upload

---
+++ Mods/awards/src/awards.lua
@@ -0,0 +1,216 @@
+
+local S = awards.gettext
+
+awards.register_award("Dig_ice_1",{
+    title = S("Dig 1 ice block"),
+    description = S("Dig 1 ice block."),
+    icon = "awards_moon.png",
+    trigger = {
+        type = "dig",
+        node = "moon_mapgen:moonstone_200",
+        target = 1
+    }
+})
+
+awards.register_award("Dig_ice_10",{
+    title = S("Dig 10 ice blocks"),
+    description = S("Dig 10 ice blocks."),
+    icon = "awards_moon.png",
+    trigger = {
+        type = "dig",
+        node = "moon_mapgen:moonstone_200",
+        target = 10
+    }
+})
+
+awards.register_award("Dig_ice_100",{
+    title = S("Dig 100 ice blocks"),
+    description = S("Dig 100 ice blocks."),
+    icon = "awards_moon.png",
+    trigger = {
+        type = "dig",
+        node = "moon_mapgen:moonstone_200",
+        target = 100
+    }
+})
+
+awards.register_award("Dig_ice_1000",{
+    title = S("Dig 1000 ice blocks"),
+    description = S("Dig 1000 ice blocks."),
+    icon = "awards_moon.png",
+    trigger = {
+        type = "dig",
+        node = "moon_mapgen:moonstone_200",
+        target = 1000
+    }
+})
+
+awards.register_award("Dig_ice_10000",{
+    title = S("Dig 10000 ice blocks"),
+    description = S("Dig 10000 ice blocks."),
+    icon = "awards_moon.png",
+    trigger = {
+        type = "dig",
+        node = "moon_mapgen:moonstone_200",
+        target = 10000
+    }
+})
+
+awards.register_award("Dig 100",{
+    title = S("Dig 100 blocks"),
+    description = S("Dig 100 blocks."),
+    prizes = "default:pick_diamond",
+    icon = "awards_moon.png",
+    trigger = {
+        type = "dig",
+        target = 100
+    }
+})
+
+awards.register_award("Dig 1000",{
+    title = S("Dig 1000 blocks"),
+    description = S("Dig 1000 blocks."),
+    prizes = "default:pick_diamond",
+    icon = "awards_moon.png",
+    trigger = {
+        type = "dig",
+        target = 1000
+    }
+})
+
+awards.register_award("Place_Molded_1",{
+    title = S("Place 1 Molded regolith blocks"),
+    description = S("Place 1 Molded regolith blocks."),
+    icon = "awards_moon.png",
+    trigger = {
+        type = "place",
+        node = "moon_mapgen:moldedregolith",
+        target = 1
+    }
+})
+
+awards.register_award("Place_Molded_10",{
+    title = S("Place 10 Molded regolith blocks"),
+    description = S("Place 10 Molded regolith blocks."),
+    icon = "awards_moon.png",
+    trigger = {
+        type = "place",
+        node = "moon_mapgen:moldedregolith",
+        target = 10
+    }
+})
+
+awards.register_award("Place_Molded_100",{
+    title = S("Place 100 Molded regolith blocks"),
+    description = S("Place 100 Molded regolith blocks."),
+    icon = "awards_moon.png",
+    trigger = {
+        type = "place",
+        node = "moon_mapgen:moldedregolith",
+        target = 100
+    }
+})
+
+awards.register_award("Place_Molded_1000",{
+    title = S("Place 1000 Molded regolith blocks"),
+    description = S("Place 1000 Molded regolith blocks."),
+    icon = "awards_moon.png",
+    trigger = {
+        type = "place",
+        node = "moon_mapgen:moldedregolith",
+        target = 1000
+    }
+})
+
+awards.register_award("Place_Molded_10000",{
+    title = S("Place 10000 Molded regolith blocks"),
+    description = S("Place 10000 Molded regolith blocks."),
+    icon = "awards_moon.png",
+    trigger = {
+        type = "place",
+        node = "moon_mapgen:moldedregolith",
+        target = 10000
+    }
+})
+
+--[[
+awards.register_award("Eat_potato_10",{
+    title = S("Eat 100 Baked potatoes"),
+    description = S("Eat 100 Baked potatoes."),
+    icon = "awards_moon.png",
+    trigger = {
+        type = "eat",
+        node = "farming:baked_potato",
+        target = 10
+    }
+})
+
+awards.register_award("Eat_potato_100",{
+    title = S("Eat 100 potatoes"),
+    description = S("Eat 100 potatoes."),
+    icon = "awards_moon.png",
+    trigger = {
+        type = "eat",
+        node = "farming:potato",
+        target = 100
+    }
+})
+
+awards.register_award("Eat_raspberries_100",{
+    title = S("Eat 100 raspberries"),
+    description = S("Eat 100 raspberries."),
+    icon = "awards_moon.png",
+    trigger = {
+        type = "eat",
+        node = "farming:raspberries",
+        target = 100
+    }
+})
+
+--[[
+awards.register_award("Eat_bread_1",{
+    title = S("Eat 1 bread"),
+    description = S("Eat 1 bread."),
+    icon = "awards_moon.png",
+    trigger = {
+        type = "eat",
+        node = "farming:bread",
+        target = 1
+    }
+})
+
+awards.register_award("Eat_breads_10",{
+    title = S("Eat 10 breads"),
+    description = S("Eat 10 breads."),
+    icon = "awards_moon.png",
+    trigger = {
+        type = "eat",
+        node = "farming:bread",
+        target = 10
+    }
+})
+
+awards.register_award("Eat_breads_100",{
+    title = S("Eat 100 breads"),
+    description = S("Eat 100 breads."),
+    icon = "awards_moon.png",
+    trigger = {
+        type = "eat",
+        node = "farming:bread",
+        target = 100
+    }
+})
+
+awards.register_award("Eat_breads_1000",{
+    title = S("Eat 1000 breads"),
+    description = S("Eat 1000 breads."),
+    icon = "awards_moon.png",
+    trigger = {
+        type = "eat",
+        node = "farming:bread",
+        target = 1000
+    }
+})
+]]--
+
+
